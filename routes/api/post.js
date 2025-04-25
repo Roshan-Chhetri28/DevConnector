71e5c5d0fff43d68ddb2e5c0b7b788f8cbd1297a
Roshan_chhetri28 <roshanchhetri931@gmail.com>
2025-04-25 10:52:31 +0530

    fix: Correct comment formatting and add comment functionality in post routes

--- routes/api/post.js
+++ routes/api/post.js
@@ -9,9 +9,9 @@ const User = require("../../modles/User")
 
 
 
-//* @route    post api/post
-//* @desc     Create a post
-//* @access   Private
+// @route    post api/post
+// @desc     Create a post
+// @access   Private
 
 router.post('/', [auth, [
     check('text', "text is required").not().isEmpty()
@@ -48,9 +48,9 @@ router.post('/', [auth, [
 
     })
 
-//* @route    GET api/post
-//* @desc     GET all post
-//* @access   Public
+// @route    GET api/post
+// @desc     GET all post
+// @access   Public
 
 router.get('/', async (req, res) => {
     try {
@@ -63,9 +63,9 @@ router.get('/', async (req, res) => {
 })
 
 
-//* @route    GET api/post/:post_id
-//* @desc     get post by post_id
-//* @access   Private
+// @route    GET api/post/:post_id
+// @desc     get post by post_id
+// @access   Private
 router.get('/:id', auth, async (req, res) => {
     try {
         const post = await Post.findById(req.params.id)
@@ -82,9 +82,9 @@ router.get('/:id', auth, async (req, res) => {
     }
 })
 
-//* @route    DELETE api/post/:post_id
-//* @desc     DELETE post by post_id
-//* @access   Private
+// @route    DELETE api/post/:post_id
+// @desc     DELETE post by post_id
+// @access   Private
 router.delete('/:id', auth, async (req, res) => {
     try {
         const post = await Post.findById(req.params.id)
@@ -102,9 +102,9 @@ router.delete('/:id', auth, async (req, res) => {
     }
 })
 
-//* @route    PUT api/post/:post_id
-//* @desc     Put Like
-//* @access   PRIVATE
+// @route    PUT api/post/:post_id
+// @desc     Put Like
+// @access   PRIVATE
 
 router.put('/like/:id', auth, async (req, res) => {
     try {
@@ -127,9 +127,9 @@ router.put('/like/:id', auth, async (req, res) => {
     }
 })
 
-//* @route    PUT api/post/unlike/like/:post_id
-//* @desc     Put Like
-//* @access   PRIVATE
+// @route    PUT api/post/unlike/like/:post_id
+// @desc     Put Like
+// @access   PRIVATE
 
 router.put('/unlike/:id', auth, async (req, res) => {
     try {
@@ -143,7 +143,7 @@ router.put('/unlike/:id', auth, async (req, res) => {
 
         //get the remove index
 
-        const removeIndex = post.likes.map(like=>like.user.toString()).indexOf(req.user.id);
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
 
 
         post.likes.splice(removeIndex, 1)
@@ -156,4 +156,81 @@ router.put('/unlike/:id', auth, async (req, res) => {
         return res.status(500).send("Internal Server Error")
     }
 })
+
+// @route    post api/post/comment/:id
+// @desc     Comment on a post
+// @access   Private
+
+router.post('/comment/:id', [auth, [
+    check('text', "text is required").not().isEmpty()
+]],
+    async (req, res) => {
+        const error = validationResult(req)
+
+        if (!error.isEmpty()) {
+            return res.status(400).json({ errors: error.array() })
+        }
+        try {
+            const user = await User.findById(req.user.id).select('-password')
+            const post = await Post.findById(req.params.id);
+
+            const newComment = {
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            }
+
+            post.comments.unshift(newComment)
+            await post.save()
+
+            res.json(post.comments)
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server Error")
+
+        }
+
+
+
+
+    }
+)
+
+
+// @route    Delete api/post/comment/:post_id/:comment_id
+// @desc     Delete Comment on a post
+// @access   Private
+router.delete('/comment/:post_id/:comment_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.post_id)
+
+        //* Find post
+        const comment = post.comments.find(comment => comment.id === req.params.comment_id)
+
+
+        //* make sure comment exist
+        if (!comment) {
+            return res.status(404).send("Comment not found")
+        }
+
+        // check user
+        if (comment.user.toString() !== req.user.id) {
+            return res.status(401).send("User not authorized")
+        }
+
+        const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+
+
+        post.comments.splice(removeIndex, 1)
+        await post.save()
+
+        res.json(post.comments)
+    } catch (err) {
+        console.error(err)
+        res.status(500).send('server Error')
+    }
+})
+
+
 module.exports = router
