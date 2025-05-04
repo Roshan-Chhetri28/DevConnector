d5b4c5fcb583a4b817488c0cffbc82aa96822f2c
Roshan_chhetri28 <roshanchhetri931@gmail.com>
2025-05-04 17:46:50 +0530

    change to relative path

--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
+const base = process.env.REACT_APP_API_URL || '';
+
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+  baseURL: `${base}/api`,
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  withCredentials: true
 });
 
 export default api;
