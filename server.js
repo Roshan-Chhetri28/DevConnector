00d83ae4b8e35d8c949c75e42214616aea374416
Roshan_chhetri28 <roshanchhetri931@gmail.com>
2025-05-04 17:05:07 +0530

    bug fix

--- server.js
+++ server.js
@@ -35,12 +35,13 @@ app.use('/api/post', require('./routes/api/post'));
 app.use('/api/auth', require('./routes/api/auth'));
 
 //serve static assets in production 
-if(process.env.NODE_ENV == 'production'){
-    //set static folder
-    app.use(express.static('client/build'))
-    app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+if (process.env.NODE_ENV === 'production') {
+    // serve React’s build folder
+    app.use(express.static('client/build'));
+
+    app.get('/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
